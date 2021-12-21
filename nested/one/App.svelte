22abb38e42810d596e61d1ef0148d729b31df727
tanzisong <tanzisong920@163.com>
2021-12-21 23:02:59 +0800

    feat: 修改配置项

---
+++ nested/one/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+</script>
+<main>
+  <p>
+   one page
+  </p>
+</main>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { resolve } from "path";
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+import {resolve} from "path";
+import legacy from "@vitejs/plugin-legacy";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [legacy({
+    targets: ['ie >= 11'],
+    additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+  }), svelte()],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'nested/index.html')
+        nested: resolve(__dirname, 'nested/index.html'),
+        one: resolve(__dirname, 'nested/one/index.html')
       }
     }
   }
